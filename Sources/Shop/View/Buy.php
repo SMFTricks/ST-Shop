0159c131deb0b2697315a61f1a54f14208702ee2
Diego Andrés <diegoandres_cortes@outlook.com>
2020-05-13 19:35:30 -0500

    view
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Sources/Shop/View/Buy.php
@@ -0,0 +1,240 @@
+<?php
+
+/**
+ * @package ST Shop
+ * @version 4.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2020, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+namespace Shop\View;
+
+use Shop\Shop;
+use Shop\Helper\Database;
+use Shop\Helper\Images;
+use Shop\Helper\Format;
+use Shop\Helper\Log;
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+class Buy
+{
+	public function main()
+	{
+		global $context, $scripturl, $modSettings, $sourcedir, $boardurl;
+
+		// Check if user is allowed to access this section
+		if (!allowedTo('shop_canManage'))
+			isAllowedTo('shop_canBuy');
+
+		// Set all the page stuff
+		require_once($sourcedir . '/Subs-List.php');
+		$context['sub_template'] = 'show_list';
+		$context['default_list'] = 'itemslist';
+		$context['page_title'] = Shop::getText('main_button') . ' - ' . Shop::getText('main_buy');
+		$context['template_layers'][] = 'options';
+		$context['linktree'][] = [
+			'url' => $scripturl . '?action=shop;sa=buy',
+			'name' => Shop::getText('main_buy'),
+		];
+		// Images...
+		$context['items_url'] = $boardurl . Shop::$itemsdir;
+		$context['shop_images_list'] = Images::list();
+		// ... and categories
+		$context['shop_categories_list'] = Database::Get(0, 1000, 'sc.name', 'shop_categories AS sc', Database::$categories);
+		$context['form_url'] = '?action=shop;sa=buy'. (isset($_REQUEST['cat']) && $_REQUEST['cat'] >= 0 ? ';cat='.$_REQUEST['cat'] : '');
+
+		// The entire list
+		$listOptions = [
+			'id' => 'itemslist',
+			//'title' => Shop::getText('main_buy'),
+			'items_per_page' => !empty($modSettings['Shop_items_perpage']) ? $modSettings['Shop_items_perpage'] : 15,
+			'base_href' => $context['form_url'],
+			'default_sort_col' => 'item_buy',
+			'default_sort_dir' => 'ASC',
+			'get_items' => [
+				'function' => 'Shop\Helper\Database::Get',
+				'params' => ['shop_items AS s', array_merge(Database::$items, ['sc.name AS category']), 'WHERE s.status = 1'. (isset($_REQUEST['cat']) && $_REQUEST['cat'] >= 0 ? ' AND s.catid = {int:cat}' : ''), false, 'LEFT JOIN {db_prefix}shop_categories AS sc ON (s.catid = sc.catid)', ['cat' => isset($_REQUEST['cat']) ? $_REQUEST['cat'] : 0]],
+			],
+			'get_count' => [
+				'function' => 'Shop\Helper\Database::Count',
+				'params' => ['shop_items AS s', Database::$items, 'WHERE s.status = 1'. (isset($_REQUEST['cat']) && $_REQUEST['cat'] >= 0 ? ' AND s.catid = {int:cat}' : ''), '', ['cat' => isset($_REQUEST['cat']) ? $_REQUEST['cat'] : 0]],
+			],
+			'no_items_label' => Shop::getText('no_items'),
+			'no_items_align' => 'center',
+			'columns' => [
+				'item_image' => [
+					'header' => [
+						'value' => Shop::getText('item_image'),
+						'class' => 'centertext',
+					],
+					'data' => [
+						'function' => function($row)
+						{
+							return Format::image($row['image']);
+						},
+						'style' => 'width: 10%',
+						'class' => 'centertext',
+					],
+				],
+				'item_name' => [
+					'header' => [
+						'value' => Shop::getText('item_name'),
+						'class' => 'lefttext',
+					],
+					'data' => [
+						'function' => function($row)
+						{
+							return '<span style="font-size:110%">'.$row['name'] .'</span>' . (!empty($row['description']) ? '<br/><span class="smalltext">' . $row['description'] . '</span>' : '');
+						},
+						'class' => 'lefttext',
+						'style' => 'width: 35%',
+					],
+					'sort' =>  [
+						'default' => 'name',
+						'reverse' => 'name DESC',
+					],
+				],
+				'item_details' => [
+					'header' => [
+						'value' => Shop::getText('item_details'),
+						'class' => 'lefttext',
+					],
+					'data' => [
+						'function' => function($row)
+						{
+							global $scripturl;
+
+							// Category
+							$details = '<strong>' . Shop::getText('item_category') . ': </strong>' . (!empty($row['catid']) ? $row['category'] : Shop::getText('item_uncategorized'));
+
+							// Stock
+							$details .= '<br><strong>' . Shop::getText('item_stock') . ': </strong>' . $row['stock'];
+
+							// Who owns this
+							$details .= '<br><a href="'. $scripturl. '?action=shop;sa=owners;id='. $row['itemid']. '">'. Shop::getText('buy_item_who_this'). '</a>';
+
+							return $details;
+						},
+						'class' => 'lefttext',
+						'style' => 'width: 20%',
+					],
+					'sort' =>  [
+						'default' => 'category DESC',
+						'reverse' => 'category',
+					],
+				],
+				'item_options' => [
+					'header' => [
+						'value' => Shop::getText('item_price'),
+						'class' => 'centertext',
+					],
+					'data' => [
+						'function' => function($row)
+						{
+							global $modSettings;
+
+							return (($row['price'] == 0) ? '<i>' .Shop::getText('item_free').'</i>' : Format::cash($row['price']));
+						},
+						'class' => 'centertext',
+					],
+					'sort' =>  [
+						'default' => 'price DESC',
+						'reverse' => 'price',
+					],
+				],
+				'item_buy' => [
+					'header' => [
+						'value' => Shop::getText('item_buy'),
+						'class' => 'centertext',
+					],
+					'data' => [
+						'function' => function($row)
+						{
+							global $context, $modSettings, $scripturl;
+
+							// If we don\'t have stock... Sold out!
+							if ($row['stock'] == 0)
+								$message = Shop::getText('buy_soldout');
+
+							// User doesn't have enough money
+							elseif ($context['user']['shopMoney'] < $row['price'])
+								$message = '<i>' . sprintf(Shop::getText('buy_notenough'), $modSettings['Shop_credits_suffix']) . '</i>';
+
+							// Enough money? Buy it!
+							else
+								$message = '<a href="'. $scripturl. '?action=shop;sa=buy2;id='. $row['itemid']. ';'. $context['session_var'] .'='. $context['session_id'] .'">'. Shop::getText('item_buy'). '</a>';
+
+							return $message;
+						},
+						'class' => 'centertext',
+					],
+					'sort' =>  [
+						'default' => 'itemid DESC',
+						'reverse' => 'itemid',
+					],
+				],
+			],
+		];
+
+		// Let's finishem
+		createList($listOptions);
+	}
+
+	public function purchase()
+	{
+		global $context, $user_info, $modSettings, $scripturl;
+
+		// Check if he is allowed to access this section
+		if (!allowedTo('shop_canManage'))
+			isAllowedTo('shop_canBuy');
+
+		// Set all the page stuff
+		$context['page_title'] = Shop::getText('main_button') . ' - ' . Shop::getText('main_buy');
+		$context['linktree'][] = [
+			'url' => $scripturl . '?action=shop;sa=buy',
+			'name' => Shop::getText('main_buy'),
+		];
+
+		checkSession('request');
+
+		// You cannot get here without an item
+		if (!isset($_REQUEST['id']) || empty($_REQUEST['id']))
+			fatal_error(Shop::getText('buy_something'), false);
+
+		// Make sure is an int
+		$id = (int) $_REQUEST['id'];
+
+		// Get the item's information
+		$context['shop']['item_details'] = Database::Get('', '', '', 'shop_items AS s', Database::$items, 'WHERE s.status = 1 AND s.itemid = {int:itemid}', true, '', ['itemid' => $id]);
+
+		// We found and item?
+		if (empty($context['shop']['item_details']))
+			fatal_error(Shop::getText('item_notfound'), false);
+
+		// We need to find out the difference if there's not enough money
+		$notenough = ($context['shop']['item_details']['price'] - $user_info['shopMoney']);
+
+		// How many of this item does the user own?
+		$limit = Database::Count('shop_inventory AS si', Database::$inventory, 'WHERE itemid = {int:id} AND userid = {int:userid}', '', ['id' => $context['shop']['item_details']['itemid'], 'userid' => $user_info['id'],]);
+
+		// Already reached the limit?
+		if ((!empty($context['shop']['item_details']['itemlimit'])) && ($limit >= $context['shop']['item_details']['itemlimit']))
+			fatal_error(Shop::getText('item_limit_reached'), false);
+		// Item valid and enabled then... Do we have items in stock?
+		elseif (empty($context['shop']['item_details']['stock']))
+			fatal_error(sprintf(Shop::getText('buy_item_nostock'), $context['shop']['item_details']['name']), false);
+		// Fine... Does the user have enough money to buy this?
+		elseif ($user_info['shopMoney'] < $context['shop']['item_details']['price'])
+			fatal_error(sprintf(Shop::getText('buy_item_notenough'), $modSettings['Shop_credits_suffix'], $context['shop']['item_details']['name'], $notenough, $modSettings['Shop_credits_prefix']), false);
+
+		// Proceed
+		// Handle item purchase and money deduction and log it
+		Log::purchase($context['shop']['item_details']['itemid'], $user_info['id'], $context['shop']['item_details']['price']);
+
+		// Redirect to the inventory?
+		redirectexit('action=shop;sa=inventory;sort=item_date;purchased');
+	}
+}
---
+++ Sources/Shop/View/Home.php
@@ -0,0 +1,433 @@
+<?php
+
+/**
+ * @package ST Shop
+ * @version 4.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2020, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+namespace Shop\View;
+
+use Shop\Shop;
+use Shop\Helper\Format;
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+class Home
+{
+	var $shop_tabs = [];
+	var $subactions = [];
+
+	function __construct()
+	{
+		// Load language files
+		loadLanguage('Shop/Shop');
+		loadLanguage('Shop/Errors');
+
+		// Load template file
+		loadTemplate('Shop/Shop');
+	}
+
+	public function main()
+	{
+		global $context, $scripturl, $modSettings, $user_info;
+
+		// Set all the page stuff
+		$context['page_title'] = Shop::getText('main_button');
+		$context['linktree'][] = array(
+			'url' => $scripturl . '?action=shop',
+			'name' => Shop::getText('main_button'),
+		);
+
+		// What if the Shop is disabled? User shouldn't be able to access the Shop
+		if (empty($modSettings['Shop_enable_shop']))
+			fatal_error(Shop::getText('currently_disabled'), false);
+
+		// Last but not less important. Are they actually allowed to Access the Shop? If not.. YOU SHALL NOT PASS. 
+		// Anyway if he can Manage the Shop, there's no problem.
+		if (!empty($modSettings['Shop_enable_shop']) && !allowedTo('shop_canAccess') && !allowedTo('shop_canManage'))
+			isAllowedTo('shop_canAccess');
+
+		// Maintenance. Only Shop admins can access if the shop it's enabled
+		if (!empty($modSettings['Shop_enable_shop']) && !empty($modSettings['Shop_enable_maintenance']) && allowedTo('shop_canAccess') && !allowedTo('shop_canManage'))
+			fatal_error(Shop::getText('currently_maintenance'), false);
+
+		// Games Pass, get the days!
+		$context['user']['gamedays'] = ($user_info['gamesPass'] <= time() || empty($user_info['gamesPass']) ? 0 : Format::gamespass($user_info['gamesPass']));
+
+		// Lovely copyright in shop pages
+		$context['shop']['copyright'] = $this->copyright();
+		// Shop tabs
+		$context['shop']['tabs'] = $this->tabs();
+		
+		// Big array of actions
+		$this->subactions = [
+			'home' =>  'Home::portal',
+			'buy' => 'Buy::main',
+			'buy2' => 'Buy::purchase',
+			'inventory' => 'Inventory::main',
+			'search' => 'Inventory::search',
+			'search2' => 'Inventory::search_inventory',
+			'invuse' => 'Inventory::use',
+			'invused' => 'Inventory::used',
+			'invfav' => 'Inventory::fav',
+			'owners' => 'Inventory::owners',
+			'invdisp' => 'Inventory::display_extend',
+			'gift' => 'Gift::main',
+			'senditem' => 'Gift::main',
+			'sendmoney' => 'Gift::main',
+			'gift2' => 'Gift::send',
+			'gift3' => 'Gift::send2',
+		];
+
+		$subactions2 = [
+			'invtrade' => array(
+				'function' => 'ShopInventory::Trade',
+			),
+			'invtrade2' => array(
+				'function' => 'ShopInventory::Trade2',
+			),
+			
+			'bank' => array(
+				'function' => 'ShopBank::Main',
+			),
+			'bank2' => array(
+				'function' => 'ShopBank::Trans',
+			),
+			'trade' => array(
+				'function' => 'ShopTrade::Main',
+			),
+			'tradelist' => array(
+				'function' => 'ShopTrade::List',
+			),
+			'mytrades' => array(
+				'function' => 'ShopTrade::Profile',
+			),
+			'tradelog' => array(
+				'function' => 'ShopTrade::Log',
+			),
+			'trade2' => array(
+				'function' => 'ShopTrade::Transaction',
+			),
+			'trade3' => array(
+				'function' => 'ShopTrade::Transaction2',
+			),
+			'traderemove' => array(
+				'function' => 'ShopTrade::Remove',
+			),
+			'tradesearch' => array(
+				'function' => 'ShopTrade::Search',
+			),
+			'stats' => array(
+				'function' => 'ShopStats::Main',
+			),
+			'games' => array(
+				'function' => 'ShopGames::Main',
+			),
+		];
+		// More sections?
+		call_integration_hook('integrate_shop_home_actions', array(&$this->subactions));
+
+		// Invoke the function
+		$sa = isset($_GET['sa'], $this->subactions[$_GET['sa']]) ? $_GET['sa'] : 'home';
+		call_helper(__NAMESPACE__ . '\\' . $this->subactions[$sa] . '#');
+	}
+
+	public function tabs()
+	{
+		$this->shop_tabs = [
+			'home' => [
+				'action' => ['home'],
+				'label' => Shop::getText('main_home'),
+				'permission' => 'shop_canAccess',
+				'enable' => 'Shop_enable_shop'
+			],
+			'buy' => [
+				'action' => ['buy','buy2','buy3', 'whohas'],
+				'label' => Shop::getText('main_buy'),
+				'permission' => 'shop_canBuy',
+				'enable' => 'Shop_enable_shop'
+			],
+			'inventory' => [
+				'action' => ['inventory', 'invdisp', 'invuse', 'invused', 'owners','search','search2'],
+				'label' => Shop::getText('main_inventory'),
+				'permission' => 'shop_canBuy',
+				'enable' => 'Shop_enable_shop'
+			],
+			'gift' => [
+				'action' => ['gift','senditem','sendmoney','gift2','gift3'],
+				'label' => Shop::getText('main_gift'),
+				'permission' => 'shop_canGift',
+				'enable' => 'Shop_enable_gift'
+			],
+			'bank' => [
+				'action' => ['bank', 'bank2'],
+				'label' => Shop::getText('main_bank'),
+				'permission' => 'shop_canBank',
+				'enable' => 'Shop_enable_bank'
+			],
+			'trade' => [
+				'action' => ['trade', 'tradelist', 'mytrades', 'tradelog', 'trade2','trade3','traderemove', 'invtrade', 'invtrade2',],
+				'label' => Shop::getText('main_trade'),
+				'permission' => 'shop_canBuy',
+				'enable' => 'Shop_enable_trade'
+			],
+			'games' => [
+				'action' => ['games'],
+				'label' => Shop::getText('main_games'),
+				'permission' => 'shop_playGames',
+				'enable' => 'Shop_enable_games'
+			],
+			'stats' => [
+				'action' => ['stats'],
+				'label' => Shop::getText('main_stats'),
+				'permission' => 'shop_viewStats',
+				'enable' => 'Shop_enable_stats'
+			],
+		];
+		// Magic tabs?
+		call_integration_hook('integrate_shop_home_tabs', [&$this->shop_tabs]);
+
+		return $this->shop_tabs;
+	}
+
+	public function portal()
+	{
+		global $context, $user_info, $modSettings, $scripturl;
+
+		// Set all the page stuff
+		$context['page_title'] = Shop::getText('main_button') . ' - ' . Shop::getText('main_home');
+		$context['sub_template'] = 'home';
+		$context['linktree'][] = array(
+			'url' => $scripturl . '?action=shop;sa=home',
+			'name' => Shop::getText('main_home'),
+		);
+
+		// Forum name + Shop
+		$context['shop']['forum_welcome'] = sprintf(Shop::getText('welcome_to'), $context['forum_name']);
+
+		// Welcome message
+		$context['shop']['welcome'] = sprintf(Shop::getText('welcome_text'), $user_info['name'], $modSettings['Shop_credits_suffix']);
+
+		// Profile action??
+		if (isset($_REQUEST['u']) && !empty($_REQUEST['u']))
+			redirectexit('action=shop;sa=gift;u='.$_REQUEST['u']);
+
+		// Display some general stats
+		/*$context['home_stats'] = array(
+			// Last items added
+			'last_added' => array(
+				'label' => $txt['Shop_stats_last_added'],
+				'icon' => 'last_added.png',
+				'function' => ShopStats::LastItems(),
+				'enabled' => true,
+			),
+			// Last items bought
+			'last_bought' => array(
+				'label' => $txt['Shop_stats_last_bought'],
+				'icon' => 'last_bought.png',
+				'function' => ShopStats::LastBought(),
+				'enabled' => allowedTo('shop_canBuy'),
+			),
+			// Richest pocket
+			'richest_pocket' => array(
+				'label' => $txt['Shop_stats_richest_pocket'],
+				'icon' => 'richest_pocket.png',
+				'function' => ShopStats::Richest('pocket'),
+				'enabled' => true,
+			),
+			// Richest bank
+			'richest_bank' => array(
+				'label' => $txt['Shop_stats_richest_bank'],
+				'icon' => 'richest_bank.png',
+				'function' => ShopStats::Richest('bank'),
+				'enabled' => allowedTo('shop_canBank') && !empty($modSettings['Shop_enable_bank']),
+			),
+		);*/
+	}
+
+
+
+	public static function logGift($userid, $receiver, $message, $amount = 0, $itemid = 0, $invid = 0)
+	{
+		global $smcFunc;
+
+		// He sent an item
+		if ($amount == 0) {
+			// Transfer the item to the new user
+			$smcFunc['db_query']('', '
+				UPDATE {db_prefix}shop_inventory
+				SET	userid = {int:receiver}
+				WHERE id = {int:invid}',
+				array(
+					'receiver' => $receiver,
+					'invid' => $invid,
+				)
+			);
+		}
+		// He sent money
+		else {
+			// Remove the money from the user pocket
+			$smcFunc['db_query']('', '
+				UPDATE {db_prefix}members
+				SET	shopMoney = shopMoney - {int:amount}
+				WHERE id_member = {int:userid}',
+				array(
+					'userid' => $userid,
+					'amount' => $amount,
+				)
+			);
+			// Add the amount to the receiver pocket
+			$smcFunc['db_query']('', '
+				UPDATE {db_prefix}members
+				SET	shopMoney = shopMoney + {int:amount}
+				WHERE id_member = {int:receiver}',
+				array(
+					'receiver' => $receiver,
+					'amount' => $amount,
+				)
+			);
+		}
+
+		// Insert the information in the log
+		$smcFunc['db_insert']('',
+			'{db_prefix}shop_log_gift',
+			array(
+				'userid' => 'int',
+				'receiver' => 'int',
+				'amount' => 'int',
+				'itemid' => 'int',
+				'invid' => 'int',
+				'message' => 'string',
+				'is_admin' => 'int',
+				'date' => 'int',
+			),
+			array(
+				$userid,
+				$receiver,
+				$amount,
+				$itemid,
+				$invid,
+				$message,
+				0,
+				time()
+			),
+			array()
+		);
+	}
+
+	public static function logBank($userid, $amount, $fee, $type = 0)
+	{
+		global $smcFunc;
+
+		// It's a deposit
+		if ($type == 0 || $type == 1)
+		{
+			// Add the amount to the user's bank and remove it from pocket
+			$smcFunc['db_query']('', '
+				UPDATE {db_prefix}members
+				SET ' . ($type == 0 ? '
+					shopBank = shopBank + {int:amount},
+					shopMoney = shopMoney - {int:amount} - {int:fee}' : '
+					shopBank = shopBank + {int:amount} - {int:fee},
+					shopMoney = shopMoney - {int:amount}'). '
+				WHERE id_member = {int:userid}',
+				array(
+					'amount' => $amount,
+					'fee' => $fee,
+					'userid' => $userid,
+				)
+			);
+		}
+		// Withdraw then
+		else {
+			// Add the amount to the user's pocket and remove it from bank
+			$smcFunc['db_query']('', '
+				UPDATE {db_prefix}members
+				SET  ' . ($type == 2 ? '
+					shopMoney = shopMoney + {int:amount} - {int:fee},
+					shopBank = shopBank - {int:amount}' : '
+					shopMoney = shopMoney + {int:amount},
+					shopBank = shopBank - {int:amount} - {int:fee}'). '
+				WHERE id_member = {int:userid}',
+				array(
+					'amount' => $amount,
+					'fee' => $fee,
+					'userid' => $userid,
+				)
+			);
+		}
+
+		// Insert the information in the log
+		$smcFunc['db_insert']('',
+			'{db_prefix}shop_log_bank',
+			array(
+				'userid' => 'int',
+				'amount' => 'int',
+				'fee' => 'int',
+				'type' => 'int',
+				'date' => 'int',
+			),
+			array(
+				$userid,
+				$amount,
+				$fee,
+				$type,
+				time()
+			),
+			array()
+		);
+	}
+
+	public static function logGames($userid, $amount, $game = 'slots')
+	{
+		global $smcFunc;
+
+		// Add/remove the amount from the player
+		$smcFunc['db_query']('', '
+			UPDATE {db_prefix}members
+			SET	shopMoney = shopMoney + {int:amount}
+			WHERE id_member = {int:userid}',
+			array(
+				'userid' => $userid,
+				'amount' => $amount,
+			)
+		);
+
+		// Insert the information in the log
+		$smcFunc['db_insert']('',
+			'{db_prefix}shop_log_games',
+			array(
+				'userid' => 'int',
+				'amount' => 'int',
+				'game' => 'string',
+				'date' => 'int',
+			),
+			array(
+				$userid,
+				$amount,
+				$game,
+				time()
+			),
+			array()
+		);
+	}
+
+
+
+	/**
+	 * Shop::copyright()
+	 *
+	 * Used in the credits action.
+	 * @param boolean $return decide between returning a string or append it to a known context var.
+	 * @return string A link for copyright notice
+	 */
+	public function copyright($return = false)
+	{
+		return '
+			<br /><div style="text-align: center;"><span class="smalltext">Powered by <a href="https://smftricks.com" target="_blank" rel="noopener">ST Shop</a></span></div>';
+	}
+}
