8ed5bdedfd946b72424e11e70d65adc7ab2decf5
Diego Andrés <diegoandres_cortes@outlook.com>
2020-05-13 19:35:04 -0500

    permissions
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Sources/Shop/Integration/Permissions.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @package ST Shop
+ * @version 4.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2020, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+namespace Shop\Integration;
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+class Permissions
+{
+	public static $shop_permissions = [
+		'shop_canAccess',
+		'shop_canBuy',
+		'shop_viewInventory',
+		'shop_canGift',
+		'shop_canTrade',
+		'shop_canBank',
+		'shop_viewStats',
+		'shop_playGames',
+		'shop_canManage'
+	];
+
+	/**
+	 * Permissions::load_permissions()
+	 *
+	 * ST Shop permissions
+	 * @param array $permissionGroups An array containing all possible permissions groups.
+	 * @param array $permissionList An associative array with all the possible permissions.
+	 * @return void
+	 */
+	public function load_permissions(&$permissionGroups, &$permissionList, &$leftPermissionGroups, &$hiddenPermissions, &$relabelPermissions)
+	{
+		global $modSettings;
+
+		// Shop permissions
+		$permissionGroups['membergroup'] = array_merge($permissionGroups['membergroup'], ['shop']);
+		foreach (self::$shop_permissions as $p)
+			$permissionList['membergroup'][$p] = [false,'shop'];
+
+		// Shop disabled? No permissions then
+		if (empty($modSettings['Shop_enable_shop']))
+			$hiddenPermissions = array_merge($hiddenPermissions, self::$shop_permissions);
+	}
+
+	/**
+	 * Permissions::illegal_guest()
+	 *
+	 * ST Shop Illegal permissions
+	 * 
+	 */
+	public function illegal_guest()
+	{
+		global $context;
+
+		// Guests do not play nicely with this mod
+		$context['non_guest_permissions'] = array_merge($context['non_guest_permissions'], self::$shop_permissions);
+	}
+}
