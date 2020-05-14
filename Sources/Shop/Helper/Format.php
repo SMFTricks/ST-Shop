b1424f214715daa335eb8e8c989c12668f81ec57
Diego Andrés <diegoandres_cortes@outlook.com>
2020-05-13 19:34:47 -0500

    helper
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ Sources/Shop/Helper/Format.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * @package ST Shop
+ * @version 4.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2020, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+namespace Shop\Helper;
+
+use Shop\Shop;
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+class Format
+{
+	private static $items_url;
+	/**
+	 * Format::cash()
+	 *
+	 * It gives the money a format, adding the suffix and prefix set in the admin (if)
+	 * @param $money A string that usually includes the amount of cash/credits
+	 * @param $formal It will put the suffix at the beginning if enabled
+	 * @return string A text containing the specified money/string with format
+	 */
+	public function cash($money, $formal = false)
+	{
+		global $modSettings;
+
+		if (empty($formal))
+			$disp = (!empty($modSettings['Shop_credits_prefix']) ? $modSettings['Shop_credits_prefix'] : '') . ($money) . ' ' . (!empty($modSettings['Shop_credits_suffix']) ? $modSettings['Shop_credits_suffix'] : '');
+		else
+			$disp = ($money) . (!empty($modSettings['Shop_credits_prefix']) ? $modSettings['Shop_credits_prefix'] : '');
+
+		return $disp;
+	}
+
+	/**
+	 * Format::item()
+	 *
+	 * Gives the provided item format with his image
+	 * @param $image The image of an item
+	 * @param $description Optional parameter for including the description in the title/alt
+	 * @param $class Optional parameter for including class and id
+	 * @return string A formatted image
+	 */
+	public function image($image, $description = '', $class = '')
+	{
+		global $modSettings, $boardurl;
+
+		// Item images...
+		self::$items_url = $boardurl . Shop::$itemsdir;
+
+		$formatname = '<img' .(!empty($class) ? ' class="'.$class.'" id="'.$class.'"' : ''). ' src="'. self::$items_url . $image. '" alt="'.$description.'" title="'.$description.'" width="'.$modSettings['Shop_images_width'].'" height="'.$modSettings['Shop_images_height'].'" style="vertical-align: middle;" />';
+
+		return $formatname;
+	}
+
+	/**
+	 * Format::gamespass()
+	 *
+	 * Provides a format to the dyas left of gamespass access
+	 * @param $days The days the user has on his subscription to the gamespass
+	 * @return string Days left of gamespass
+	 */
+	public function gamespass($days)
+	{
+		return round((($days - time()) / 86400));
+	}
+}
---
+++ Sources/Shop/Helper/Log.php
@@ -0,0 +1,129 @@
+<?php
+
+/**
+ * @package ST Shop
+ * @version 4.0
+ * @author Diego Andrés <diegoandres_cortes@outlook.com>
+ * @copyright Copyright (c) 2020, SMF Tricks
+ * @license https://www.mozilla.org/en-US/MPL/2.0/
+ */
+
+namespace Shop\Helper;
+
+use Shop\Shop;
+
+if (!defined('SMF'))
+	die('No direct access...');
+
+class Log
+{
+	private static $gifts = [
+		'userid' => 'int',
+		'receiver' => 'int',
+		'amount' => 'int',
+		'itemid' => 'int',
+		'invid' => 'int',
+		'message' => 'string',
+		'is_admin' => 'int',
+		'date' => 'int',
+	];
+	private static $buy = [
+		'itemid' => 'int',
+		'invid' => 'int',
+		'userid' => 'int',
+		'sellerid' => 'int',
+		'amount' => 'int',
+		'fee' => 'int',
+		'date' => 'int',
+	];
+	private static $inventory = [
+		'userid' => 'int',
+		'itemid' => 'int',
+		'trading' => 'int',
+		'tradecost' => 'int',
+		'date' => 'int',
+		'tradedate' => 'int',
+		'fav' => 'int',
+	];
+	private static $types = [];
+
+	public function credits($sender, $users, $amount, $admin = false, $message = '')
+	{
+		// Send credits over to the user
+		Database::Update('members', ['users' => $users, 'credits' => $amount], 'shopMoney = shopMoney + {int:credits},', 'WHERE id_member' . (is_array($users) ? ' IN ({array_int:users})' : ' = {int:users}'));
+
+		// Log the information
+		if (is_array($users))
+			foreach ($users as $memID)
+				Database::Insert('shop_log_gift', [
+					$sender,
+					$memID,
+					$amount,
+					0,
+					0,
+					$message,
+					!empty($admin) ? 1 : 0,
+					time()
+				], self::$gifts);
+		// single user
+		else
+			Database::Insert('shop_log_gift', [
+				$sender,
+				$users,
+				$amount,
+				0,
+				0,
+				$message,
+				!empty($admin) ? 1 : 0,
+				time()
+			], self::$gifts);
+
+		// Regular user?
+		if (empty($admin))
+			Database::Update('members', ['user' => $sender, 'credits' => $amount], 'shopMoney = shopMoney - {int:credits}', 'WHERE id_member = {int:user}');
+	}
+
+	public function purchase($itemid, $userid, $amount, $seller = 0, $fee = 0, $invid = 0)
+	{
+		// Remove the credits from the buyer
+		Database::Update('members', ['user' => $userid, 'credits' => $amount], 'shopMoney = shopMoney - {int:credits},', 'WHERE id_member = {int:user}');
+
+		// Is he purchasing an item fro items list?
+		if (empty($seller))
+		{
+			// Insert in inventory
+			Database::Insert('shop_inventory', [
+				$userid,
+				$itemid,
+				0,
+				0,
+				time(),
+				0,
+				0
+			], self::$inventory);
+
+			// Discount stock
+			Database::Update('shop_items', ['count' => 1, 'itemid' => $itemid], 'stock = stock - {int:count},', 'WHERE itemid = {int:itemid}');
+		}
+		// Purchasing at the trade center
+		else
+		{
+			// Move item to buyer inventory
+			Database::Update('shop_inventory', ['user' => $userid, 'invid' => $invid, 'date' => time()], 'userid = {int:user}, trading = 0, tradecost = 0, tradedate = 0, date = {int:date},', 'WHERE id = {int:invid}');
+
+			// Add the credits to the seller
+			Database::Update('members', ['user' => $seller, 'paid' => $amount, 'fee' => $fee], 'shopMoney = shopMoney + ({int:paid} - {int:fee}),', 'WHERE id_member = {int:user}');
+		}
+
+		// Insert info in the log
+		Database::Insert('shop_log_buy', [
+			$itemid,
+			$invid,
+			$userid,
+			$seller,
+			$amount,
+			$fee,
+			time()
+		], self::$buy);
+	}
+}
