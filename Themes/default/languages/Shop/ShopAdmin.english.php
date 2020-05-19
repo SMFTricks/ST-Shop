68e80c3c2ab7996ee880c5702e11c893b2ecc352
Diego Andrés <diegoandres_cortes@outlook.com>
2020-05-18 20:00:36 -0500

    corrected inventory
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/languages/Shop/ShopAdmin.english.php
+++ Themes/default/languages/Shop/ShopAdmin.english.php
@@ -278,42 +278,35 @@ $txt['Shop_logs_updated'] = 'The selected entries were successfully deleted.';
 // Admin inventory
 $txt['Shop_tab_inventory'] = 'Shop Inventory';
 $txt['Shop_tab_inventory_desc'] = 'Here you can search user\'s inventory and delete items from their profile.';
+$txt['Shop_inventory_members_desc'] = 'Here you can select the members you wish to send stuff to.';
 $txt['Shop_inventory_userinv'] = 'Browse inventory';
 $txt['Shop_inventory_search'] = 'Search inventory';
+$txt['Shop_inventory_items_deleted'] = 'The selected items were successfully deleted from %s\'s profile';
+$txt['Shop_inventory_credits_updated'] = 'The credits of %s were successfully updated';
 $txt['Shop_inventory_groupcredits'] = 'Group credits';
 $txt['Shop_inventory_groupcredits_desc'] = 'Send credits to specific membergroups.';
 $txt['Shop_inventory_groupcredits_success'] = (!empty($modSettings['Shop_credits_suffix']) ? $modSettings['Shop_credits_suffix'] : 'Credits') . ' were successfully sent to the selected groups.';
-
+$txt['Shop_inventory_groupcredits_membergroup'] = 'Membergroup';
+$txt['Shop_inventory_groupcredits_action'] = 'Action:';
+$txt['Shop_inventory_groupcredits_add'] = 'Add';
+$txt['Shop_inventory_groupcredits_substract'] = 'Substract';
 $txt['Shop_inventory_usercredits'] = 'Send credits';
 $txt['Shop_inventory_usercredits_desc'] = 'Send credits to specific members in the forum.';
 $txt['Shop_inventory_usercredits_success'] = $modSettings['Shop_credits_suffix'] . ' were successfully sent to the selected users.';
-
-
-
+$txt['Shop_inventory_useritems'] = 'Send items';
+$txt['Shop_inventory_useritems_desc'] = 'Send items to specific members in the forum. If any item has a specific carrying limit it will be ignored, but it will still lower the stock for the item on the number of members selected.';
+$txt['Shop_inventory_useritems_success'] = 'Selected item was successfully sent';
 $txt['Shop_inventory_restock'] = 'Restock items';
 $txt['Shop_inventory_restock_desc'] = 'Here you can reset the stock of specific items, or all of them.';
-$txt['Shop_user_credits_updated'] = 'The credits of %s were successfully updated';
-$txt['Shop_inventory_items_deleted'] = 'The selected items were successfully deleted from %s\'s profile';
-$txt['Shop_groupcredits_sent'] = (!empty($modSettings['Shop_credits_suffix']) ? $modSettings['Shop_credits_suffix'] : 'Credits') . ' were successfully sent';
-$txt['Shop_membergroup'] = 'Membergroup';
-$txt['Shop_groupcredits_action'] = 'Action:';
-$txt['Shop_groupcredits_add'] = 'Add';
-$txt['Shop_groupcredits_substract'] = 'Substract';
-$txt['Shop_restock_successful'] = 'Selected items were restocked successfully';
-$txt['Shop_restock_what'] = 'Items to restock';
-$txt['Shop_restock_all'] = 'Restock all items';
-$txt['Shop_restock_selected'] = 'Restock only selected items';
-$txt['Shop_restock_select_items'] = 'Select the items you would like to restock';
-$txt['Shop_restock_lessthan'] = 'Restock all items with a stock less or equal to';
-$txt['Shop_restock_lessthan_desc'] = 'If you checked the option to restock only selected items, this option will be ignored.';
-$txt['Shop_restock_amount'] = 'Amount to add to stock';
-$txt['Shop_restock_amount_desc'] = 'It will sum that number with the current stock of the item(s)';
-
-$txt['Shop_inventory_members_desc'] = 'Here you can select the members you wish to send stuff to.';
-$txt['Shop_inventory_useritems'] = 'Send items';
-$txt['Shop_inventory_useritems_desc'] = 'Send items to specific members in the forum. <br> If any item has a specific carrying limit it will be ignored, but it will still lower the stock for the item on the number of members selected.';
-$txt['Shop_useritems_sent'] = 'Selected item was successfully sent';
-
+$txt['Shop_inventory_restock_success'] = 'Selected items were successfully restocked';
+$txt['Shop_inventory_restock_what'] = 'Items to restock';
+$txt['Shop_inventory_restock_all'] = 'Restock all items';
+$txt['Shop_inventory_restock_selected'] = 'Restock only selected items';
+$txt['Shop_inventory_restock_select_items'] = 'Select the items you would like to restock';
+$txt['Shop_inventory_restock_lessthan'] = 'Restock all items with a stock less or equal to';
+$txt['Shop_inventory_restock_lessthan_desc'] = 'If you checked the option to restock only selected items, this option will be ignored.';
+$txt['Shop_inventory_restock_amount'] = 'Amount to add to stock';
+$txt['Shop_inventory_restock_amount_desc'] = 'It will sum that number with the current stock of the item(s)';
 
 // Files
 $txt['Shop_file_already_exists'] = 'Sorry, that file already exists.';
