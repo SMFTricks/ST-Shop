8f7bba60bd715e4a5df34bb45b5b0e428992a58e
Diego Andrés <diegoandres_cortes@outlook.com>
2020-10-30 22:01:06 -0600

    add comment block
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Sources/Shop/Integration/Signup.php
+++ Sources/Shop/Integration/Signup.php
@@ -15,6 +15,14 @@ if (!defined('SMF'))
 
 class Signup
 {
+	/**
+	 * Signup::register()
+	 *
+	 * Gives money toe the users upon registration
+	 * 
+	 * @param array $regOptions An array of registration options
+	 * @return void
+	 */
 	public function register(&$regOptions, &$theme_vars, &$knownInts, &$knownFloats)
 	{
 		global $modSettings;
