c35f5658eab45d34a8e372ad016a03c5ded0a7aa
Diego Andrés <diegoandres_cortes@outlook.com>
2020-11-05 16:27:10 -0600

    wip
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Sources/Shop/Integration/Likes.php
+++ Sources/Shop/Integration/Likes.php
@@ -23,58 +23,31 @@ class Likes
 	private $_credits_like;
 
 	/**
-	 * @var array Store the data about the specific message id
+	 * @var int ID of the post author
 	 */
-	private $_likedAuthor;
-
-	/**
-	 * Posting::__construct()
-	 *
-	 * Add default values for the posting values, and create a new instance of Boards
-	 */
-	function __construct()
-	{
-		// Some defaults for fallback
-		$this->_credits_like = 0;
-	}
+	private $_author;
 
 	/**
 	 * Likes::likePost()
 	 *
-	 * Gives or removes points to author of the post for each like.
+	 * Gives points to author of the post for each like.
 	 * @return void
 	 */
-	public function likePost($like_info)
+	public function likePost(&$type, &$content, &$user, &$time)
 	{
 		global $modSettings;
 
 		//Are we giving credits per like?
 		if (!empty($modSettings['Shop_credits_likes_post']))
 		{
-			// Set the amount of credits
-			$this->_credits_like = $modSettings['Shop_credits_likes_post'];
-
 			// We are only interested in messages for now
-			if ($this->_likes_class::get($like_info->_type) == 'msg')
+			if ($type == 'msg')
 			{
-				$this->_likedAuthor = Database::Get('', '', '', 'messages', ['id_member'], 'WHERE id_msg = {int:like}', true, ['like' => $like_info->_content]);
+				// For some reason likes don't provide the id of the content author, only the one liking
+				$this->_author = Database::Get('', '', '', 'messages', ['id_member'], 'WHERE id_msg = {int:id_msg}', true, '', ['id_msg' => $content]);
 
-				// Like removed, points too!
-				if ($this->_likes_class::get($like_info->_alreadyLiked))
-				{
-					Database::Update('members', [
-						'likepost' => $this->_credits_like,
-						'id_member' => $this->_likedAuthor['id_member'],
-					], 'SET shopMoney = shopMoney - {int:likepost}', 'WHERE id_member = {int:id_member}');
-				}
 				// Post liked, points delivered!
-				else
-				{
-					Database::Update('members', [
-						'likepost' => $this->_credits_like,
-						'id_member' => $this->_likedAuthor['id_member'],
-					], 'SET shopMoney = shopMoney + {int:likepost}', 'WHERE id_member = {int:id_member}');
-				}
+				Database::Update('members', ['likepost' => $modSettings['Shop_credits_likes_post'], 'id_member' => $this->_author['id_member']], 'shopMoney = shopMoney + {int:likepost},', 'WHERE id_member = {int:id_member}');
 			}
 		}
 	}
