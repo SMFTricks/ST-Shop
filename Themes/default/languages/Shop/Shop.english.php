506e65c054a2f839bd154a5f6ff6452e16deea52
Diego Andrés <diegoandres_cortes@outlook.com>
2022-11-02 21:59:50 -0600

    Move who strings to their own file
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

--- Themes/default/languages/Shop/Shop.english.php
+++ Themes/default/languages/Shop/Shop.english.php
@@ -196,35 +196,3 @@ $txt['Shop_stats_top_profit'] = 'Top profit';
 $txt['Shop_stats_top_spent'] = 'Top spent';
 $txt['Shop_stats_most_expensive'] = 'Most expensive';
 $txt['Shop_stats_nostats'] = 'There are currently no stats to display.';
-
-// Shop who
-$txt['whoallow_shopinfo'] = 'Managing the Shop in the Admin Center';
-$txt['whoallow_shopsettings'] = 'Managing the Shop Settings';
-$txt['whoallow_shopitems'] = 'Managing the Shop Items';
-$txt['whoadmin_shopmodules'] = 'Managing the Shop Modules';
-$txt['whoallow_shopcategories'] = 'Managing the Shop Categories';
-$txt['whoallow_shopgames'] = 'Managing the Shop Games';
-$txt['whoallow_shopinventory'] = 'Managing the Shop Inventories and Stock';
-$txt['whoallow_shoplogs'] = 'Viewing the Shop logs';
-$txt['whoallow_shop'] = 'Viewing the <a href="' . $scripturl . '?action=shop">Forum Shop</a>';
-
-// Shop actions
-$txt['Shop_who_buy'] = 'Buying items in the <a href="' . $scripturl . '?action=shop;sa=buy">Forum Shop</a>';
-$txt['Shop_who_gift'] = 'Sending a <a href="' . $scripturl . '?action=shop;sa=gift">Gift</a> through the <a href="' . $scripturl . '?action=shop">Forum Shop</a>';
-$txt['Shop_who_sendmoney'] = 'Sending <a href="' . $scripturl . '?action=shop;sa=sendmoney">%s</a> through the <a href="' . $scripturl . '?action=shop">Forum Shop</a>';
-$txt['Shop_who_owninventory'] = 'Viewing <a href="' . $scripturl . '?action=shop;sa=inventory;u=%1$d">their own Shop Inventory</a>';
-$txt['Shop_who_inventory'] = 'Viewing <a href="' . $scripturl . '?action=shop;sa=inventory;u=%1$d">%2$s</a>\'s Shop Inventory';
-$txt['Shop_who_search'] = 'Searching for someone\'s <a href="' . $scripturl . '?action=shop;sa=search">Inventory</a>';
-$txt['Shop_who_bank'] = 'Making a transaction in the <a href="' . $scripturl . '?action=shop;sa=bank">Shop Bank</a>';
-$txt['Shop_who_trade'] = 'Viewing the <a href="' . $scripturl . '?action=shop;sa=trade">Trade Center</a>';
-$txt['Shop_who_tradelist'] = 'Viewing the <a href="' . $scripturl . '?action=shop;sa=tradelist">Trade List</a> at the <a href="' . $scripturl . '?action=shop;sa=trade">Trade Center</a>';
-$txt['Shop_who_tradelist_other'] = 'Viewing <a href="' . $scripturl . '?action=shop;sa=tradelist;u=%1$d">%2$s\'s Trade List</a> in the <a href="' . $scripturl . '?action=shop;sa=trade">Trade Center</a>';
-$txt['Shop_who_tradelist_own'] = 'Viewing <a href="' . $scripturl . '?action=shop;sa=tradelist;u=%1$d"">their own Trade List</a> <a href="' . $scripturl . '?action=shop;sa=trade">Trade Center</a>';
-$txt['Shop_who_tradelog'] = 'Viewing their trade log in the <a href="' . $scripturl . '?action=shop;sa=trade">Trade Center</a>';
-$txt['Shop_who_stats'] = 'Viewing the <a href="' . $scripturl . '?action=shop;sa=stats">Shop Stats</a>';
-$txt['Shop_who_games'] = 'Playing in the <a href="' . $scripturl . '?action=shop;sa=games">Games Room</a>';
-$txt['Shop_who_games_slots'] = 'Playing <a href="'. $scripturl. '?action=shop;sa=games;play=slots">Slots</a> in the <a href="' . $scripturl . '?action=shop;sa=games">Games Room</a>';
-$txt['Shop_who_games_lucky2'] = 'Playing <a href="'. $scripturl. '?action=shop;sa=games;play=lucky2">Lucky2</a> in the <a href="' . $scripturl . '?action=shop;sa=games">Games Room</a>';
-$txt['Shop_who_games_number'] = 'Playing <a href="'. $scripturl. '?action=shop;sa=games;play=number">Number Slots</a> in the <a href="' . $scripturl . '?action=shop;sa=games">Games Room</a>';
-$txt['Shop_who_games_pairs'] = 'Playing <a href="'. $scripturl. '?action=shop;sa=games;play=pairs">Pairs</a> in the <a href="' . $scripturl . '?action=shop;sa=games">Games Room</a>';
-$txt['Shop_who_games_dice'] = 'Rolling the <a href="'. $scripturl. '?action=shop;sa=games;play=dice">Dice</a> in the <a href="' . $scripturl . '?action=shop;sa=games">Games Room</a>';
